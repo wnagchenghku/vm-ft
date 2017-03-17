ad70348a184a7ca79fdb4005805828eaa72eb74c
Michael Xusheng Chen <michael.xschen@gmail.com>
2017-03-17 18:43:53 +0800

    remove some debug msg

--- qemu/migration/ram.c
+++ qemu/migration/ram.c
@@ -3157,11 +3157,13 @@ void colo_flush_ram_cache(void)
             memcpy(dst_host, src_host, TARGET_PAGE_SIZE);
         }
     }
+    printf("\n\n\n*****\ncolo result: host_dirty=%"PRIu64" both dirty=%"PRIu64"\n\n*****\n", host_dirty, both_dirty);
+
+    
     rcu_read_unlock();
     assert(migration_dirty_pages == 0);
     trace_colo_flush_ram_cache_begin(host_dirty);
     trace_colo_flush_ram_cache_begin(both_dirty);
-    printf("\n\n\n*****\ncolo result: host_dirty=%"PRIu64" both dirty=%"PRIu64"\n\n*****\n", host_dirty, both_dirty);
     fflush(stdout);
     trace_colo_flush_ram_cache_end();
 }
