e2c34b5842a8e1c2a07e5b01973924838039c976
Your Name <you@example.com>
2017-03-24 12:16:50 +0800

    get qlen

--- qemu/migration/output-buffer.c
+++ qemu/migration/output-buffer.c
@@ -368,3 +368,8 @@ int mc_flush_oldest_buffer(void)
 
     return mc_deliver(1);
 }
+
+uint64_t mc_get_qlen(void)
+{
+    return rtnl_tc_get_stat((struct rtnl_tc *) qdisc, RTNL_TC_QLEN);
+}
--- qemu/migration/output-buffer.h
+++ qemu/migration/output-buffer.h
@@ -4,5 +4,6 @@
 int mc_enable_buffering(void);
 int mc_start_buffer(void);
 int mc_flush_oldest_buffer(void);
+uint64_t mc_get_qlen(void);
 
 #endif /* QEMU_OUTPUT_BUFFER_H */
