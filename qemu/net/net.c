6a94750be3dc8e559189a366a94234b54e3399cc
Michael Xusheng Chen <michael.xschen@gmail.com>
2017-03-24 13:27:05 +0800

    nit

--- qemu/net/net.c
+++ qemu/net/net.c
@@ -87,7 +87,7 @@ unsigned long get_and_rest_output_counter(void){
 }
 
 
-static void count_payload_length(const uint8_t* buf, int len, int dir){
+static void count_payload_length(const uint8_t* buf, int len, int dir, unsigned flags, char* sender_name){
     int eth_hdr_len = sizeof(struct ether_header);
 
     if (len > eth_hdr_len){
@@ -107,8 +107,8 @@ static void count_payload_length(const uint8_t* buf, int len, int dir){
                 struct in_addr dstad;
                 dstad.s_addr = ip_header-> ip_dst.s_addr; 
 
-                fprintf(stderr, "payload_length = %d, src_ip = %s:%d", payload_length, inet_ntoa(srcad), htons(tcp_header->th_sport));
-                fprintf(stderr, "dst_ip = %s:%d, dir = %d\n", inet_ntoa(dstad),htons(tcp_header->th_dport), dir);
+                fprintf(stderr, "payload_length = %d, src_ip = %s:%d  ", payload_length, inet_ntoa(srcad), htons(tcp_header->th_sport));
+                fprintf(stderr, "dst_ip = %s:%d, dir = %d, flags = %u, sedner.name =%s \n", inet_ntoa(dstad),htons(tcp_header->th_dport), dir, flags, sender_name);
                 pthread_spin_lock(&counter_lock);
                 output_counter = output_counter + payload_length;
                 pthread_spin_unlock(&counter_lock);                
@@ -658,7 +658,7 @@ static ssize_t filter_receive_iov(NetClientState *nc,
             }
         }
     } else {
-        count_payload_length((uint8_t *) iov[0].iov_base, iov[0].iov_len, direction);
+        count_payload_length((uint8_t *) iov[0].iov_base, iov[0].iov_len, direction, flags, sender->name);
         //fprintf(stderr, "got output, iovcnt=%d\n", iovcnt);
         QTAILQ_FOREACH_REVERSE(nf, &nc->filters, NetFilterHead, next) {
             ret = qemu_netfilter_receive(nf, direction, sender, flags, iov,
