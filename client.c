8522f771cfd287fa74710dca5f295d2e6c133078
wangchenghku <wangchenghku@gmail.com>
2017-04-12 14:02:57 +0800

    Update client.c

--- client.c
+++ client.c
@@ -39,9 +39,9 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    printf("Please enter the message: ");
+    //printf("Please enter the message: ");
     bzero(buffer,256);
-    fgets(buffer,255,stdin);
+    //fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0) 
          error("ERROR writing to socket");
