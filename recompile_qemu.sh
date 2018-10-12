f5c4cc1a804228546011ccf20f58ea32dcd56d23
hkucs <hkucs@heming-rdma2.cs.hku.hk>
2018-10-12 20:46:23 +0800

    recompile_qemu.sh: Improve

--- recompile_qemu.sh
+++ recompile_qemu.sh
@@ -1,7 +1,13 @@
-cd ~/vm-ft/rdma-paxos/target
-make
-cd ~/vm-ft/qemu
+# pull latest code
+cd ~/vm-ft
 git reset 6db228f3775c6d230b8c30b84e8b4c0b29ff8857 --hard
 git pull origin boxuan/failover
+
+# recompile rdma-paxos
+cd ~/vm-ft/rdma-paxos/target
+make
+
+# recompile qemu
+cd ~/vm-ft/qemu
 sudo make -j`nproc`
 cd ~
